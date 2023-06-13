89153f7dc76f17f7e4cd9c1717a6b39f8f4038e5
vgn-1 <109327302+vgn-1@users.noreply.github.com>
2023-06-13 17:55:16 +1000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,1 @@
-resource "aws_s3_bucket" "example" {
-  bucket = "my-tf-test-bucket-vgn-1-hello"
 
-  tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
-    RandomKey = "RandomValue"
-  }
-}
-resource "aws_ssm_parameter" "foo" {
-  name  = "foo"
-  type  = "String"
-  value = "barr"
-}
