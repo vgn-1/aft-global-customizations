55501b67ee620148f1b8f47ab8e2896caa3d2429
vgn-1 <109327302+vgn-1@users.noreply.github.com>
2023-06-13 16:03:08 +1000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,8 @@
-resource "aws_s3_account_public_access_block" "block_all_public" {
-  block_public_acls   = true
-  block_public_policy = true
-  ignore_public_acls  = true
-  restrict_public_buckets  = true
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket-vgn-1-hello"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
